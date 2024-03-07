2be89cf27979fc95dcb7ed02b6a67977e20b3ead
Fahrel09 <fahrelrhozianikrodhipratama@smkwikrama.sch.id>
2024-03-07 13:53:46 +0700

    p

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './assets/css/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Hallo</h1>
+  )
+}
+
+export default App
